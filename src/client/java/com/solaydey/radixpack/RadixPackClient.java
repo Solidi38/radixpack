65b8c980c35cf3735a10dc7f368f1a089b5230bc
Solidi38 <68111643+Solidi38@users.noreply.github.com>
2025-11-03 19:48:03 +0100

    First Setup

---
+++ src/client/java/com/solaydey/radixpack/RadixPackClient.java
@@ -0,0 +1,10 @@
+package com.solaydey.radixpack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RadixPackClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
